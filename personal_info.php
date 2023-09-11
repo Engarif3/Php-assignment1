d57079ed2e6c7514587d770cb8805dfac34a2726
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-09-11 17:51:54 +0200

    first task done

---
+++ personal_info.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Md Arifur Rahman";
+$age = "30";
+$country = "Bangladesh";
+$introduction = "I am pursuing master's in Automotive Software Engineering at Chemnitz University of Technology in Germany. I have done my bachelor in Electrical and Electronic Engineering at Ahsanullah University of Science and Technology";
+
+echo ("My name is ".$name."\n"."I am ".$age." years old"."\n"."I am from ".$country."\n".$introduction);
